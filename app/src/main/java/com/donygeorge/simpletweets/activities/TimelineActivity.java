f716dbe59960865d8356c060940a7c1f1f4e5d59
Dony George <donygeorge@gmail.com>
2017-09-30 13:55:57 -0700

    Initial commit: video walthrough

---
+++ app/src/main/java/com/donygeorge/simpletweets/activities/TimelineActivity.java
@@ -0,0 +1,85 @@
+package com.donygeorge.simpletweets.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.donygeorge.simpletweets.R;
+import com.donygeorge.simpletweets.TwitterApplication;
+import com.donygeorge.simpletweets.TwitterClient;
+import com.donygeorge.simpletweets.adapters.TweetAdapter;
+import com.donygeorge.simpletweets.models.Tweet;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cz.msebera.android.httpclient.Header;
+
+public class TimelineActivity extends AppCompatActivity {
+
+    private TwitterClient mClient;
+    private TweetAdapter mAdapter;
+    private ArrayList<Tweet> mTweets;
+
+    @BindView(R.id.rvTweets)
+    RecyclerView rvTweets;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+        ButterKnife.bind(this);
+
+        mClient = TwitterApplication.getRestClient();
+        mTweets = new ArrayList<>();
+        mAdapter = new TweetAdapter(mTweets);
+        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+        rvTweets.setAdapter(mAdapter);
+
+        populateTimeline();
+    }
+
+    private void populateTimeline() {
+        mClient.getHomeTimeline(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                for (int i = 0; i < response.length(); i ++) {
+                    try {
+                        Tweet tweet = Tweet.fromJSON(response.getJSONObject(i));
+                        mTweets.add(tweet);
+                        mAdapter.notifyItemInserted(mTweets.size() - 1);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/donygeorge/simpletweets/adapters/TweetAdapter.java
@@ -0,0 +1,71 @@
+package com.donygeorge.simpletweets.adapters;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.donygeorge.simpletweets.R;
+import com.donygeorge.simpletweets.models.Tweet;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> {
+
+    private List<Tweet> mTweets;
+    private Context mContext;
+
+    public TweetAdapter(List<Tweet> tweets) {
+        mTweets = tweets;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        mContext = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+
+        View tweetView = inflater.inflate(R.layout.item_tweet, parent, false);
+        ViewHolder viewHolder = new ViewHolder(tweetView);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Tweet tweet = mTweets.get(position);
+
+        holder.tvUserName.setText(tweet.user.name);
+        holder.tvBody.setText(tweet.body);
+        Glide.with(mContext)
+                .load(tweet.user.profileImageUrl)
+                .into(holder.ivProfileImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTweets.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.ivProfileImage)
+        ImageView ivProfileImage;
+        @BindView(R.id.tvUserName)
+        TextView tvUserName;
+        @BindView(R.id.tvBody)
+        TextView tvBody;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
