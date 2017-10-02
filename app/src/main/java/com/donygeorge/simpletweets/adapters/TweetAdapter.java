e97b710a96530a07eb4a37a8f1aa1a09b9ff29b8
Dony George <donygeorge@gmail.com>
2017-10-01 18:16:33 -0700

    Style Dialog

--- app/src/main/java/com/donygeorge/simpletweets/adapters/TweetAdapter.java
+++ app/src/main/java/com/donygeorge/simpletweets/adapters/TweetAdapter.java
@@ -4,7 +4,6 @@ package com.donygeorge.simpletweets.adapters;
 import android.content.Context;
 import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
